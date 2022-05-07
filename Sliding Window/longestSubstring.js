194af85c1bdb50e96b0dc2f787070a502077dda0
Malcolm Lord <malcolmrlord@gmail.com>
2022-05-07 12:03:54 -0500

    longest substring pt 2

--- Sliding Window/longestSubstring.js
+++ Sliding Window/longestSubstring.js
@@ -30,26 +30,21 @@ console.log(lengthOfLongestSubstring("abcabcbb"))
 ///METHOD 2
 
 var lengthOfLongestSubstring = function(s){
-    if(s.length === 0){
-        return 0
-    }
-    let maxLongest = 1;
-    let temp = "";
-    for (let i = 0; i < s.length; i++){
-        console.log(`i is ${i}`)
-        temp = s[i]
-        for(let j = i+1; j < s.length; j++){
-            console.log(`j is ${j}`)
-            if(!temp.includes(s[j])){
-                temp += s[j];
-                maxLongest = Math.max(maxLongest, temp.length)
-            }else {
-                break;
+    var lengthOfLongestSubstring = function(s) {
+        let left = 0;
+        let set = new Set();
+        let maxLength = 0;
+        
+        for(let right = 0; right < s.length; right++){
+            let currentChar = s[right];
+            while(set.has(currentChar)){
+                set.delete(s[left]);
+                left++
             }
-            console.log(`the max longest is ${maxLongest}`);
-            console.log(`the temp is ${temp}`)
+            set.add(currentChar);
+            maxLength = Math.max(maxLength, right - left + 1);
         }
+        return maxLength;
     }
-    return maxLongest;
 };
 
